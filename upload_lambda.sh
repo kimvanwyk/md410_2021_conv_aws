88f31ec1911dc4b6f441e68ec15f52fe50bc4f06
Kim van Wyk <kimv@sahomeloans.com>
2019-10-19 22:48:32 +0200

    Add email sending

--- upload_lambda.sh
+++ upload_lambda.sh
@@ -1,2 +1,2 @@
-zip -j lambda.zip app/reg_form_data_create.py
+zip -j lambda.zip app/reg_form_data_create.py app/send_email.py
 aws lambda update-function-code --function-name reg_form_data_create --zip-file fileb://lambda.zip
