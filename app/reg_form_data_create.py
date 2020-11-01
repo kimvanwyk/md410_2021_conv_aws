af41e4b21b0c893631d7ff450f798707fd626a22
Kim van Wyk <kimv@sahomeloans.com>
2020-11-01 13:55:52 +0200

    2021 changes

--- app/reg_form_data_create.py
+++ app/reg_form_data_create.py
@@ -5,55 +5,40 @@ import send_email
 
 import boto3
 
-def update_reg_num(num=1):
-    client = boto3.client('dynamodb')
+
+def update_reg_num():
+    client = boto3.client("dynamodb")
     response = client.update_item(
-    TableName='MDConv2020RefNum',
-    Key={
-        'ID': {
-            'N': '1'
+        TableName="MDConv2021RefNum",
+        Key={"ID": {"N": "1"}},
+        ReturnValues="ALL_NEW",
+        UpdateExpression="SET RefNum = RefNum + :r",
+        ExpressionAttributeValues={
+            ":r": {
+                "N": "1",
             }
         },
-    ReturnValues='ALL_NEW',
-    UpdateExpression='SET RefNum = RefNum + :r',
-    ExpressionAttributeValues={
-        ':r': {
-            'N': "1",
-            }
-        }
     )
-    reg_num = int(list(response['Attributes']['RefNum'].values())[0])
-    if num > 1:
-        client.update_item(
-        TableName='MDConv2020RefNum',
-        Key={
-            'ID': {
-                'N': '1'
-                }
-            },
-        ReturnValues='ALL_NEW',
-        UpdateExpression='SET RefNum = RefNum + :r',
-        ExpressionAttributeValues={
-            ':r': {
-                'N': f"{num-1}",
-                }
-            }
-        )
+    reg_num = int(list(response["Attributes"]["RefNum"].values())[0])
     return reg_num
+
 
 def lambda_handler(event, context):
     try:
-        s3_client = boto3.client('s3')
-        data = json.loads(event['body'])['data']
-        num = 1 if data['partner'] == 'partner_none' else 2
+        s3_client = boto3.client("s3")
+        data = json.loads(event["body"])["data"]
 
-        reg_num = update_reg_num(num)
-        data['registration_number'] = reg_num
-        data['timestamp'] = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=2))).strftime('%y/%m/%d %H:%M:%S')
-        fn = f'reg_forms/{reg_num:03}/data.json'
-        s3_client.put_object(Body=bytes(json.dumps(data), encoding='utf-8'), 
-                          Bucket='md410-2020-conv', 
-                          Key=fn)
+        reg_num = update_reg_num()
+        data["registration_number"] = reg_num
+        data["timestamp"] = datetime.datetime.now(
+            datetime.timezone(datetime.timedelta(hours=2))
+        ).strftime("%y/%m/%d %H:%M:%S")
+        fn = f"reg_forms/{reg_num:03}/data.json"
+        s3_client.put_object(
+            Body=bytes(json.dumps(data), encoding="utf-8"),
+            Bucket="md410-2021-conv",
+            Key=fn,
+        )
         send_email.send_email(reg_num)
         return {"statusCode": 200, "body": json.dumps({"reg_num": reg_num})}
     except Exception as e:
--- app/send_email.py
+++ app/send_email.py
@@ -1,32 +1,35 @@
 import boto3
 
-if __name__ == '__main__':
-    session = boto3.Session(profile_name='mdconv2020_s3')
-    client = session.client('ses', region_name='eu-west-1')
+if __name__ == "__main__":
+    session = boto3.Session(profile_name="mdconv2020_s3")
+    client = session.client("ses", region_name="eu-west-1")
 else:
-    client = boto3.client('ses')
+    client = boto3.client("ses")
+
 
 def send_email(reg_num):
     response = client.send_email(
-        Source='admin@lionsconvention2020.co.za',
+        Source="admin@lionsconvention2021.co.za",
         Destination={
-            'ToAddresses': [
-                'vanwykk+mdc2020@gmail.com',
+            "ToAddresses": [
+                "vanwykk+mdc2021@gmail.com",
             ],
         },
         Message={
-            'Subject': {
-                'Data': f'MDC 2020 Registration Form Received: {reg_num:04}',
+            "Subject": {
+                "Data": f"MDC 2021 Registration Form Received: {reg_num:03}",
             },
-            'Body': {
-                'Text': {
-                    'Data': f'MDC 2020 Registration Form Received: {reg_num:04}',
+            "Body": {
+                "Text": {
+                    "Data": f"MDC 2021 Registration Form Received: {reg_num:03}",
                 }
-            }
+            },
         },
     )
     return response
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     import sys
+
     send_email(int(sys.argv[1]))
